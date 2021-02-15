ac8311e95947cf66c6368361c8b7f1a58dd20f18
Ryan <bugnir1@gmail.com>
2021-02-15 11:11:14 -0800

    pairing with kevin

--- db/migrate/20210212224948_add_videogames_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddVideogamesToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :game_name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_210935) do
+ActiveRecord::Schema.define(version: 2021_02_13_010343) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2021_02_12_210935) do
     t.integer "number_of_players"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_videogames_on_user_id"
   end
 
+  add_foreign_key "videogames", "users"
 end
