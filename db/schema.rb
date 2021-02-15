478783e8140af131c9b063bf7614d99bbde495cd
Ryan <bugnir1@gmail.com>
2021-02-15 14:20:53 -0800

    migrated db with users_videogame

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_215428) do
+ActiveRecord::Schema.define(version: 2021_02_15_221806) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email_address"
     t.string "steam_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users_videogames", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "videogames_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
