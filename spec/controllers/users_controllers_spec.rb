be880aff85a8fcb10329c890a6de1e68febd0013
Sarah Llewelyn <se.llewelyn@gmail.com>
2021-02-26 19:05:29 -0500

    got a test working

--- spec/controllers/users_controllers_spec.rb
+++ spec/controllers/users_controllers_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-    context "GET #index" do
+
+  describe "GET #index" do
         it "returns the proper view" do
             get :index
             # expect(response).to be_success
-            expect(response).to render_template(:index) 
+            expect(response).to render_template(:index)
         end
        
     end
-
 end
