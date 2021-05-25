352e0769f77b8f4d43cdd1c95adeffe57d259cf6
Ryan <bugnir1@gmail.com>
2021-05-25 11:46:11 -0700

    changed rail to be api only in config

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module GameWithFriends
   class Application < Rails::Application
+    config.api_only = true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
